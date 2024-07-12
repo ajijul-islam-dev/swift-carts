34da42c86c842767081e89611605295d63e69df2
ajijul-islam-dev <ajijulislam5938@gmail.com>
2024-07-12 02:48:01 -0700

    initial commit with header

---
+++ app/page.tsx
@@ -0,0 +1,10 @@
+import { Button } from "@nextui-org/button";
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <main className="max-w-7xl px-5 mx-auto">
+      <section>Banner</section>
+    </main>
+  );
+}
